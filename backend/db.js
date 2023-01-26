d7fa2e0a603215a4c239162b890d3407b4ec21d5
Monimrowka <116653626+Monimrowka@users.noreply.github.com>
2023-01-26 14:47:03 +0100

    Backend

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    connectionString:
+    "postgres://pebiuhcr:JuFD4T7bOEPFLJTCeW2AKABQOCBgn4g8@kandula.db.elephantsql.com/pebiuhcr",
+});
+
+
+module.exports = { pool };
---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const db = require('../backend/db');
+const express = require('express');
+const { pool } = require("../backend/db")
+const cors = require("cors")
+const app = express();
+const PORT = 3003;
+
+
+app.use(cors())
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Hello from Homepage.'));
+
+app.get("/todos", async (req, res) => {
+
+    try {
+       const todos = await pool.query(`SELECT * FROM todos;`);
+       console.log(todos);
+       res.json(todos.rows);
+     } catch (error) {
+       res.status(500).send("Oops something went wrong");
+     }
+    
+   })
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
